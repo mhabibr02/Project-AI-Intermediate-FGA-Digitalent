0416bf8357027c4db0417103e97a8d5dac6177dc
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 14:27:08 +0700

    Update CurrencyConverter.cs

--- Plugins/ConvertCurrency/CurrencyConverter.cs
+++ Plugins/ConvertCurrency/CurrencyConverter.cs
@@ -2,10 +2,31 @@ using AITravelAgent;
 
 class CurrencyConverter
 {
-    [KernelFunction, 
-    Description("Convert an amount from one currency to another")]
-    public static string ConvertAmount()
+    [KernelFunction, Description("Convert an amount from one currency to another")]
+public static string ConvertAmount(
+    [Description("The target currency code")] string targetCurrencyCode, 
+    [Description("The amount to convert")] string amount, 
+    [Description("The starting currency code")] string baseCurrencyCode)
+{
+    var currencyDictionary = Currency.Currencies;
+
+    Currency targetCurrency = currencyDictionary[targetCurrencyCode];
+    Currency baseCurrency = currencyDictionary[baseCurrencyCode];
+
+    if (targetCurrency == null)
     {
-        var currencyDictionary = Currency.Currencies;
+        return targetCurrencyCode + " was not found";
     }
+    else if (baseCurrency == null)
+    {
+        return baseCurrencyCode + " was not found";
+    }
+    else
+    {
+        double amountInUSD = Double.Parse(amount) * baseCurrency.USDPerUnit;
+        double result = amountInUSD * targetCurrency.UnitsPerUSD;
+        return @$"${amount} {baseCurrencyCode} is approximately 
+            {result.ToString("C")} in {targetCurrency.Name}s ({targetCurrencyCode})";
+    }
+}
 }
