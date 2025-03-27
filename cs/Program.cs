00046b17222ff224df266b390d7def0d412bc274
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 13:59:43 +0700

    Update Program.cs

--- cs/Program.cs
+++ cs/Program.cs
@@ -11,3 +11,17 @@ builder.AddAzureOpenAIChatCompletion(
     "your-api-key",
     "deployment-model");
 var kernel = builder.Build();
+
+kernel.ImportPluginFromType<ConversationSummaryPlugin>();
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/TravelPlugins");
+
+ChatHistory history = [];
+string input = @"I'm planning an anniversary trip with my spouse. We like hiking, 
+    mountains, and beaches. Our travel budget is $15000";
+
+var result = await kernel.InvokeAsync<string>(prompts["SuggestDestinations"],
+    new() {{ "input", input }});
+
+Console.WriteLine(result);
+history.AddUserMessage(input);
+history.AddAssistantMessage(result);
