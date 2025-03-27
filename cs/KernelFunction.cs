0f2d77bb62e94f58d5eaad3b86ed7b4a0db10aa6
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 14:02:07 +0700

    Create KernelFunction.cs

---
+++ cs/KernelFunction.cs
@@ -0,0 +1,7 @@
+[KernelFunction, Description("Convert an amount of currency to USD")]
+public static string ConvertCurrency(
+  [Description("The currency")] string currency, 
+  [Description("The amount")] double amount)
+{
+  // Code to convert currency
+}
