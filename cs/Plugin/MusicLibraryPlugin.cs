3a524139090d9a4be2ff4913e98958cc32d1768b
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 14:06:24 +0700

    Create MusicLibraryPlugin.cs

---
+++ cs/Plugin/MusicLibraryPlugin.cs
@@ -0,0 +1,24 @@
+[KernelFunction, Description("Add a song to the recently played list")]
+public static string AddToRecentlyPlayed(
+    [Description("The name of the artist")] string artist, 
+    [Description("The title of the song")] string song, 
+    [Description("The song genre")] string genre)
+{
+    // Read the existing content from the file
+    string filePath = "data/recentlyplayed.txt";
+    string jsonContent = File.ReadAllText(filePath);
+    var recentlyPlayed = (JsonArray) JsonNode.Parse(jsonContent);
+
+    var newSong = new JsonObject
+    {
+        ["title"] = song,
+        ["artist"] = artist,
+        ["genre"] = genre
+    };
+
+    recentlyPlayed.Insert(0, newSong);
+    File.WriteAllText(filePath, JsonSerializer.Serialize(recentlyPlayed,
+        new JsonSerializerOptions { WriteIndented = true }));
+
+    return $"Added '{song}' to recently played";
+}
