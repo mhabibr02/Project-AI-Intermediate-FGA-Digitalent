954586512e3cf26b6b2a9d5ee810de4362066741
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 13:29:35 +0700

    Create Program.cs

---
+++ cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+string filePath = Path.GetFullPath("../../appsettings.json");
+var config = new ConfigurationBuilder()
+    .AddJsonFile(filePath)
+    .Build();
+
+// Set your values in appsettings.json
+string modelId = config["modelId"]!;
+string endpoint = config["endpoint"]!;
+string apiKey = config["apiKey"]!;
