7aaf9f294387f5ae194a0ac78f2ef2d5ee842597
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-03-27 14:23:08 +0700

    Update Program.cs

--- cs/Program.cs
+++ cs/Program.cs
@@ -23,9 +23,18 @@ OpenAIPromptExecutionSettings settings = new()
     ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
 };
 
-string prompt = @"I live in Portland OR USA. Based on my recently 
-    played songs and a list of upcoming concerts, which concert 
-    do you recommend?";
+var songSuggesterFunction = kernel.CreateFunctionFromPrompt(
+    promptTemplate: @"Based on the user's recently played music:
+        {{$recentlyPlayedSongs}}
+        recommend a song to the user from the music library:
+        {{$musicLibrary}}",
+    functionName: "SuggestSong",
+    description: "Recommend a song from the library"
+);
+
+kernel.Plugins.AddFromFunctions("SuggestSong", [songSuggesterFunction]);
+
+string prompt = @"Can you recommend a song from the music library?";
 
 var result = await kernel.InvokePromptAsync(prompt, new(settings));
 
